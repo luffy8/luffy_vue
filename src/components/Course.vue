90d220e29b6c728a00b818927edc2bab1c642a6a
To-Heavn <ziawang2017@outlook.com>
2018-02-28 12:02:15 +0800

    普通课程列表页面和详细页面完成

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -1,37 +1,43 @@
 <template>
   <div>
-    <ul>
-      <li v-for="item in courseList" >
-        <router-link :to="{'path':'/course-detail/'+item.id }">{{item.name}}</router-link>
-      </li>
-    </ul>
+    <div v-for="item in courseList">
+      <router-link :to="{'path':'/course-detail/'+item.id }">{{item.name}}</router-link>
+        <ul>
+          <li>
+            {{ item.level }}
+          </li>
+          <li>
+            {{ item.brief }}
+          </li>
+        </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      courseList: [
-      ]
-    }
-  },
-  mounted: function () {
-    this.initCourses()
-  },
-  methods: {
-    initCourses:function () {
-      var that = this
-      this.$axios.request({
-        url: 'http://127.0.0.1:8081/courses/',
-        method: 'GET'
-      }).then(function (response) {
-        that.courseList = response.data.courseList
-      })
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        courseList: undefined
+      }
+    },
+    mounted: function () {
+      this.initCourses()
+    },
+    methods: {
+      initCourses: function () {
+        var that = this;
+        this.$axios.request({
+          url: 'http://127.0.0.1:8081/courses/',
+          method: 'GET'
+        }).then(function (response) {
+          that.courseList = response.data;
+          console.log(response.data)
+        })
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
